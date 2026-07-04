e193a580e534883fd9d457d8221cc3e6f807b07e
LeopoldKainz <98959508+leopoldKainz@users.noreply.github.com>
2026-07-04 20:56:28 +0200

    Update App.tsx to make Create a note bigger

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ export default function App() {
       <h1>🔒 Secret Notes</h1>
 
       <section className="card">
-        <h2>Create a note</h2>
+        <h1>Create a note</h1>
         <form onSubmit={handleCreate}>
           <input
             placeholder="Title (optional)"
